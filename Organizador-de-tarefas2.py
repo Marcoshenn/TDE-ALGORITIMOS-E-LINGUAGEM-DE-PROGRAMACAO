35a0a2b54087be1ec44c93ae706d93e9e57f1079
Arthur Barboza <arthurbarbozagois.gois@gmail.com>
2025-12-04 23:12:19 -0300

    Create Organizador-de-tarefas2.py
    O nvo trecho desse código é responsável por marcar uma tarefa como concluída dentro do sistema.

---
+++ Organizador-de-tarefas2.py
@@ -0,0 +1,37 @@
+class Tarefa:
+    def __init__(self, id, descricao):
+        self.id = id
+        self.descricao = descricao
+        self.status = "pendente"
+
+class SistemaTarefas:
+    def __init__(self):
+        self.tarefas = []
+        self.id_atual = 1
+
+    def adicionar_tarefas(self):
+        descricao = input("Digite a descrição da tarefa: ")
+        tarefa = Tarefa(self.id_atual, descricao)
+        self.tarefas.append(tarefa)
+        self.id_atual += 1
+        print("Tarefa adicionada com sucesso!")
+
+    def listar_tarefas(self):
+        if not self.tarefas:
+            print("Nenhuma tarefa encontrada.")
+        else:
+            for tarefa in self.tarefas:
+                print(f"ID:{tarefa.id}, Descrição: {tarefa.descricao}, Status: {tarefa.status}")
+
+    def marcar_concluida(self):
+        try:
+            id_tarefa = int(input("Digite o ID da tarefa a ser marcada como concluída: "))
+        except ValueError:
+            print("ID inválido! Digite um número inteiro.")
+            return
+        for tarefa in self.tarefas:
+            if tarefa.id == id_tarefa:
+                tarefa.status = "concluída"
+                print("Tarefa marcada como concluída!")
+                return
+        print("Tarefa não encontrada.")
